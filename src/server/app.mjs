4a69eb57536dcd065e3cfb97d78231774fd4700c
David Campbell <davidthecomposer@gmail.com>
2020-07-13 14:52:40 -0600

    updated app.mjs settings

--- src/server/app.mjs
+++ src/server/app.mjs
@@ -5,7 +5,7 @@ import { dirname } from "path";
 import bodyParser from "body-parser";
 import { randomProfileImage } from "./randomProfileImage.mjs";
 import helmet from "helmet";
-import cors from "cors";
+// import cors from "cors";
 // import buildAllNestedObjects from "./helpers.mjs";
 import {
 	createNewComment,
@@ -22,7 +22,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 
 mongoose
 	.connect(process.env.MONGODB_URI, {
@@ -32,130 +32,133 @@ mongoose
 	})
 	.then((result) =>
 		app.listen(process.env.PORT || 8080, () => {
-			console.log(`server started on port 3000. Connected to Atlas`);
+			console.log(`server started on port 3000. Connected to mLab`);
 		})
+
+
+		app.get("/ping", function (req, res) {
+			return res.send("pong");
+		});
+		
+		app.get("/", (req, res) => {
+			res.sendFile(`${__dirname}/build/index.html`);
+		});
+		
+		app.post("/", async (req, res, error) => {
+			try {
+				const modelName = allModels[req.body.dbToQuery];
+		
+				const result = await modelName.find({}).lean();
+				result.forEach((comment) => (comment._id = comment._id.toString()));
+		
+				res.json(result);
+			} catch {
+				console.log(error);
+			}
+		});
+		
+		app.post("/new", async (req, res, error) => {
+			try {
+				const modelName = allModels[req.body.dbToQuery];
+		
+				const singleMessageObject = req.body;
+				singleMessageObject.profileImage = randomProfileImage();
+				singleMessageObject.currentVoteTally = 0;
+				singleMessageObject.allChildComments = 0;
+				console.log(singleMessageObject);
+				const {
+					name,
+					date,
+					message,
+					currentVoteTally,
+					profileImage,
+					allChildComments,
+				} = singleMessageObject;
+		
+				await createNewComment(
+					modelName,
+					name,
+					date,
+					message,
+					currentVoteTally,
+					profileImage,
+					allChildComments
+				);
+				const result = await modelName.find().lean();
+				// result.forEach((comment) => (comment._id = comment._id.toString()));
+		
+				// buildAllNestedObjects(result);
+				res.json(result);
+			} catch {
+				console.log(error);
+			}
+		});
+		
+		app.post("/reply", async (req, res, error) => {
+			try {
+				const modelName = allModels[req.body.dbToQuery];
+		
+				const singleMessageObject = req.body;
+				singleMessageObject.profileImage = randomProfileImage();
+				singleMessageObject.currentVoteTally = 0;
+				singleMessageObject.allChildComments = 0;
+				const {
+					name,
+					date,
+					message,
+					currentVoteTally,
+					profileImage,
+					parent_id,
+					allChildComments,
+				} = singleMessageObject;
+		
+				await createNewReply(
+					modelName,
+					name,
+					date,
+					message,
+					currentVoteTally,
+					profileImage,
+					parent_id,
+					allChildComments
+				);
+		
+				const result = await modelName.find().lean();
+				// result.forEach((comment) => (comment._id = comment._id.toString()));
+				// buildAllNestedObjects(result);
+				res.json(result);
+			} catch {
+				console.log(error);
+			}
+		});
+		
+		app.post("/vote", async (req, res, error) => {
+			try {
+				const modelName = allModels[req.body.dbToQuery];
+		
+				const vote = req.body;
+		
+				const { currentVoteTally, _id } = vote;
+		
+				modelName.findByIdAndUpdate(
+					{ _id },
+					{ currentVoteTally: currentVoteTally },
+					(err) => {
+						console.log(err);
+					}
+				);
+		
+				const result = await modelName.find().lean();
+				// result.forEach((comment) => (comment._id = comment._id.toString()));
+		
+				res.json(result);
+			} catch {
+				console.log(error);
+			}
+		});
+		
 	)
 	.catch((err) => console.log(err));
 
 // mongoose.ObjectId.get((v) => v.toString());
 
-app.get("/ping", function (req, res) {
-	return res.send("pong");
-});
-
-app.get("/", (req, res) => {
-	res.sendFile(`${__dirname}/build/index.html`);
-});
-
-app.post("/", async (req, res, error) => {
-	try {
-		const modelName = allModels[req.body.dbToQuery];
-
-		const result = await modelName.find({}).lean();
-		result.forEach((comment) => (comment._id = comment._id.toString()));
-
-		res.json(result);
-	} catch {
-		console.log(error);
-	}
-});
-
-app.post("/new", async (req, res, error) => {
-	try {
-		const modelName = allModels[req.body.dbToQuery];
-
-		const singleMessageObject = req.body;
-		singleMessageObject.profileImage = randomProfileImage();
-		singleMessageObject.currentVoteTally = 0;
-		singleMessageObject.allChildComments = 0;
-		console.log(singleMessageObject);
-		const {
-			name,
-			date,
-			message,
-			currentVoteTally,
-			profileImage,
-			allChildComments,
-		} = singleMessageObject;
-
-		await createNewComment(
-			modelName,
-			name,
-			date,
-			message,
-			currentVoteTally,
-			profileImage,
-			allChildComments
-		);
-		const result = await modelName.find().lean();
-		// result.forEach((comment) => (comment._id = comment._id.toString()));
-
-		// buildAllNestedObjects(result);
-		res.json(result);
-	} catch {
-		console.log(error);
-	}
-});
-
-app.post("/reply", async (req, res, error) => {
-	try {
-		const modelName = allModels[req.body.dbToQuery];
-
-		const singleMessageObject = req.body;
-		singleMessageObject.profileImage = randomProfileImage();
-		singleMessageObject.currentVoteTally = 0;
-		singleMessageObject.allChildComments = 0;
-		const {
-			name,
-			date,
-			message,
-			currentVoteTally,
-			profileImage,
-			parent_id,
-			allChildComments,
-		} = singleMessageObject;
-
-		await createNewReply(
-			modelName,
-			name,
-			date,
-			message,
-			currentVoteTally,
-			profileImage,
-			parent_id,
-			allChildComments
-		);
-
-		const result = await modelName.find().lean();
-		// result.forEach((comment) => (comment._id = comment._id.toString()));
-		// buildAllNestedObjects(result);
-		res.json(result);
-	} catch {
-		console.log(error);
-	}
-});
-
-app.post("/vote", async (req, res, error) => {
-	try {
-		const modelName = allModels[req.body.dbToQuery];
-
-		const vote = req.body;
-
-		const { currentVoteTally, _id } = vote;
-
-		modelName.findByIdAndUpdate(
-			{ _id },
-			{ currentVoteTally: currentVoteTally },
-			(err) => {
-				console.log(err);
-			}
-		);
-
-		const result = await modelName.find().lean();
-		// result.forEach((comment) => (comment._id = comment._id.toString()));
-
-		res.json(result);
-	} catch {
-		console.log(error);
-	}
-});
