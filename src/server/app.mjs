026e06b0a01ab1b82c25a4ec9377ba8c5b3c5209
David Campbell <davidthecomposer@gmail.com>
2020-07-14 17:33:37 -0600

    updated configs

--- src/server/app.mjs
+++ src/server/app.mjs
@@ -36,6 +36,14 @@ mongoose
 	)
 	.catch((err) => console.log(err));
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build/"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 app.get("/ping", function (req, res) {
 	return res.send("pong");
 });
