e29b753e1b6e15c8134681e5328292935189ef4a
David Campbell <davidthecomposer@gmail.com>
2020-07-15 07:58:16 -0600

    config changes for heroku

--- src/server/app.mjs
+++ src/server/app.mjs
@@ -37,11 +37,9 @@ mongoose
 	.catch((err) => console.log(err));
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static("__dirname/build"));
 
-	app.get("*", function (req, res) {
-		res.sendFile(path.join(__dirname, "build", "index.html"));
-	});
+	console.log("using static");
 }
 
 app.get("/ping", function (req, res) {
