4114d51f97cc13c0106d81a7d3186f2cf029d4b1
David Campbell <davidthecomposer@gmail.com>
2020-07-15 07:51:10 -0600

    updated some configs for heroku

--- src/server/app.mjs
+++ src/server/app.mjs
@@ -37,7 +37,7 @@ mongoose
 	.catch((err) => console.log(err));
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.resolve(__dirname, "build")));
+	app.use(express.static("client/build"));
 
 	app.get("*", function (req, res) {
 		res.sendFile(path.join(__dirname, "build", "index.html"));
