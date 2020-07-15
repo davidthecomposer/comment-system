680304315e01b8c684e0f2c8531eafe4dd33b3a7
David Campbell <davidthecomposer@gmail.com>
2020-07-14 20:01:13 -0600

    updated static settings

--- src/server/app.mjs
+++ src/server/app.mjs
@@ -36,6 +36,14 @@ mongoose
 	)
 	.catch((err) => console.log(err));
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.resolve(__dirname, "build")));
+
+	app.get("*", function (req, res) {
+		res.sendFile(path.join(__dirname, "build", "index.html"));
+	});
+}
+
 app.get("/ping", function (req, res) {
 	return res.send("pong");
 });
